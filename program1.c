be0145ffe22c664692848a0156cbf3788023ea69
surasri-devi <202217b3313@wilp.bits-pilani.ac.in>
2024-07-21 22:22:01 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#define MAX 10
+/* Define the required functions to create a list, insert into the list,
+delete an element from the list, search and display the list */
+void create();
+void insert();
+void deletion();
+void search();
+void display();
+int a,b[20], n, p, e, f, i, pos;
+void main()
